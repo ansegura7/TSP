e36c0a142a3c01d94cfe80a2037053dd955efb98
ansegura7 <seguraandres7@gmail.com>
2020-01-12 10:28:38 -0500

    Update TSP Solver
    Updating SOMAlgorithm tabular format.

--- code/TSP_Solver/src/tsp/algorithm/SOMAlgorithm.java
+++ code/TSP_Solver/src/tsp/algorithm/SOMAlgorithm.java
@@ -68,29 +68,29 @@ public class SOMAlgorithm extends TspAlgorithm
     // This is the method that solves the TSP problem using SOM algorithm
     public void solve()
     {
-        if (doublePoint != null) {
-        	elapsedTime = System.nanoTime();
-        	
-        	int nIter;
-        	while (graph.size() < nPoints * 2)
-            {
-        		nIter = (int)(graph.size() * MAX_ITERATIONS / nPoints);
-                for (int i = 0; i < nIter; i++)
-                	updateSOM();
-                
-                addNodes();
-                if (display)
-                	paint();
-            }
-        	
-        	finish();
-        	elapsedTime = ((System.nanoTime() - elapsedTime) / 1000000);
-        	
-        	// Show final results
-            paint();
-            if (env != null)
-            	env.displayResults();
-        }
+    	if (doublePoint != null) {
+    		elapsedTime = System.nanoTime();
+    		
+    		int nIter;
+    		while (graph.size() < nPoints * 2)
+    		{
+    			nIter = (int)(graph.size() * MAX_ITERATIONS / nPoints);
+    			for (int i = 0; i < nIter; i++)
+    				updateSOM();
+    			
+    			addNodes();
+    			if (display)
+    				paint();
+    		}
+    		
+    		finish();
+    		elapsedTime = ((System.nanoTime() - elapsedTime) / 1000000);
+    		
+    		// Show final results
+    		paint();
+    		if (env != null)
+    			env.displayResults();
+    	}
     }
     
     // Method that calculates the length of the tour
@@ -102,7 +102,7 @@ public class SOMAlgorithm extends TspAlgorithm
     	do
     	{
     		tour += getEuclideanDistance(node, node.next);
-            node = node.next;
+    		node = node.next;
     	}
     	while (node != graph.getLast());
     	
@@ -118,192 +118,192 @@ public class SOMAlgorithm extends TspAlgorithm
     // Method that looks for the initial position of the SOM
     private void setInitialPoints()
     {
-        if (doublePoint == null)
-            return;
-        
-        double cx = 0;
-        double cy = 0;
-        for (int i = 0; i < nPoints; i++)
-        {
-            cx += doublePoint[i].x;
-            cy += doublePoint[i].y;
-        }
-        cx /= nPoints;
-        cy /= nPoints;
-        
-        // Search limit nodes
-        DoublePoint nNorth = new DoublePoint(cx, cy * 0.8);
-        DoublePoint nEast = new DoublePoint(cx * 1.2, cy);
-        DoublePoint nSouth = new DoublePoint(cx, cy * 1.2);
-        DoublePoint nWest = new DoublePoint(cx * 0.8, cy);
-        
-        // Save graph data
-        graph = new DoubleLinkedList();
-        graph.rightInsert(new Node(0, nNorth), null);
-        graph.rightInsert(new Node(1, nEast), graph.get(0));
-        graph.rightInsert(new Node(2, nSouth), graph.get(1));
-        graph.rightInsert(new Node(3, nWest), graph.get(2));
-        
-        // Show SOM
-        if (display)
-        	paint();
+    	if (doublePoint == null)
+    		return;
+    	
+    	double cx = 0;
+    	double cy = 0;
+    	for (int i = 0; i < nPoints; i++)
+    	{
+    		cx += doublePoint[i].x;
+    		cy += doublePoint[i].y;
+    	}
+    	cx /= nPoints;
+    	cy /= nPoints;
+    	
+    	// Search limit nodes
+    	DoublePoint nNorth = new DoublePoint(cx, cy * 0.8);
+    	DoublePoint nEast = new DoublePoint(cx * 1.2, cy);
+    	DoublePoint nSouth = new DoublePoint(cx, cy * 1.2);
+    	DoublePoint nWest = new DoublePoint(cx * 0.8, cy);
+    	
+    	// Save graph data
+    	graph = new DoubleLinkedList();
+    	graph.rightInsert(new Node(0, nNorth), null);
+    	graph.rightInsert(new Node(1, nEast), graph.get(0));
+    	graph.rightInsert(new Node(2, nSouth), graph.get(1));
+    	graph.rightInsert(new Node(3, nWest), graph.get(2));
+    	
+    	// Show SOM
+    	if (display)
+    		paint();
     }
     
     // Method in charge of drawing. Use a separate thread to make the drawings
     private void paint()
     {
     	if (env != null) {
-	        new PaintProcess(env.envCanvas, doublePoint, graph);
-	        try { sleep(PAINT_WAIT); } catch(Exception e) { System.out.println(" - Error: " + e.getMessage()); }
+    		new PaintProcess(env.envCanvas, doublePoint, graph);
+    		try { sleep(PAINT_WAIT); } catch(Exception e) { System.out.println(" - Error: " + e.getMessage()); }
     	}
     }
     
     // Method that update the SOM
     private void updateSOM()
     {
-        int winnerNode = -1;
-        int targetNode = rnd.nextInt(nPoints);
-        
-        if (cache.containsKey(targetNode)) {
-        	winnerNode = cache.get(targetNode);
-        	cache.remove(targetNode);
-        }
-        else
-        {
-            double winnerDist = Double.MAX_VALUE;
-            double localDist = 0.0d;
-            Node node = graph.getLast();
-            
-            // Search for the node closest to that impulse
-            do
-            {
-            	localDist = getEuclideanDistance(doublePoint[targetNode], node);
-                if (localDist < winnerDist)
-                {
-                	winnerDist = localDist;  
-                    winnerNode = node.keyName;
-                }
-                node = node.next;
-            }
-            while (node != graph.getLast());
-            
-            cache.put(targetNode, winnerNode);
-        }
-        
-        // Move the winning node and give it its prize
-        graph.get(winnerNode).victories++;
-        closeupPhase(winnerNode, targetNode);
+    	int winnerNode = -1;
+    	int targetNode = rnd.nextInt(nPoints);
+    	
+    	if (cache.containsKey(targetNode)) {
+    		winnerNode = cache.get(targetNode);
+    		cache.remove(targetNode);
+    	}
+    	else
+    	{
+    		double winnerDist = Double.MAX_VALUE;
+    		double localDist = 0.0d;
+    		Node node = graph.getLast();
+    		
+    		// Search for the node closest to that impulse
+    		do
+    		{
+    			localDist = getEuclideanDistance(doublePoint[targetNode], node);
+    			if (localDist < winnerDist)
+    			{
+    				winnerDist = localDist;
+    				winnerNode = node.keyName;
+    			}
+    			node = node.next;
+    		}
+    		while (node != graph.getLast());
+    		
+    		cache.put(targetNode, winnerNode);
+    	}
+    	
+    	// Move the winning node and give it its prize
+    	graph.get(winnerNode).victories++;
+    	closeupPhase(winnerNode, targetNode);
     }
     
     // Method of the network nodes insertion
     private void addNodes()
-    {   
+    {
     	Node winner = null;
     	Node node = graph.getLast();
     	int maxVictories = 0;
-        
-        do
-        {
-            if (node.victories > maxVictories)
-            {
-            	maxVictories = node.victories;
-                winner = node;
-            }
-            node = node.next;
-        }
-        while (node != graph.getLast());
-        
-        winner.victories = 0;
-        
-        // A new node is inserted to the right
-        double x = (winner.x + winner.next.x) / 2;
-        double y = (winner.y + winner.next.y) / 2;
-        graph.rightInsert(new Node(graph.size(), 0, x, y), winner);
-        
-        // A new node is inserted to the left
-        x = (winner.x + winner.previous.x) / 2;
-        y = (winner.y + winner.previous.y) / 2;
-        graph.leftInsert(new Node(graph.size(), 0, x, y), winner);
+    	
+    	do
+    	{
+    		if (node.victories > maxVictories)
+    		{
+    			maxVictories = node.victories;
+    			winner = node;
+    		}
+    		node = node.next;
+    	}
+    	while (node != graph.getLast());
+    	
+    	winner.victories = 0;
+    	
+    	// A new node is inserted to the right
+    	double x = (winner.x + winner.next.x) / 2;
+    	double y = (winner.y + winner.next.y) / 2;
+    	graph.rightInsert(new Node(graph.size(), 0, x, y), winner);
+    	
+    	// A new node is inserted to the left
+    	x = (winner.x + winner.previous.x) / 2;
+    	y = (winner.y + winner.previous.y) / 2;
+    	graph.leftInsert(new Node(graph.size(), 0, x, y), winner);
     }
     
     // This method ends the TSP. Check which stimuli need a new node
     private void finish()
     {
     	// Local variables
-        double distUP = 0.0d;
-        double dist = 0.0d;
-        int key = 0;
-        Node node = null;
-        
-        for (int i = 0; i < nPoints; i++)
-        {
-            if (cache.containsKey(i) && !graph.get(cache.get(i)).assigned) {
-            	key = cache.get(i);
-            }
-            else
-            {
-                node = graph.getLast();
-                distUP = Double.MAX_VALUE;
-                do
-                {
-                    if (!node.assigned)
-                    {
-                        dist = getEuclideanDistance(doublePoint[i], node);
-                        if (dist < distUP)
-                        {
-                            distUP = dist;
-                            key = node.keyName;
-                        }
-                    }
-                    node = node.next;
-                }
-                while (node != graph.getLast());
-            }
-            
-            // Update
-            node = graph.get(key);
-            node.x = doublePoint[i].x;
-            node.y = doublePoint[i].y;
-            node.assigned = true;
-        }
-        
-        // Finally, remove isolated nodes from the graph
-        removeIsolatedNodes();
+    	double distUP = 0.0d;
+    	double dist = 0.0d;
+    	int key = 0;
+    	Node node = null;
+    	
+    	for (int i = 0; i < nPoints; i++)
+    	{
+    		if (cache.containsKey(i) && !graph.get(cache.get(i)).assigned) {
+    			key = cache.get(i);
+    		}
+    		else
+    		{
+    			node = graph.getLast();
+    			distUP = Double.MAX_VALUE;
+    			do
+    			{
+    				if (!node.assigned)
+    				{
+    					dist = getEuclideanDistance(doublePoint[i], node);
+    					if (dist < distUP)
+    					{
+    						distUP = dist;
+    						key = node.keyName;
+    					}
+    				}
+    				node = node.next;
+    			}
+    			while (node != graph.getLast());
+    		}
+    		
+    		// Update
+    		node = graph.get(key);
+    		node.x = doublePoint[i].x;
+    		node.y = doublePoint[i].y;
+    		node.assigned = true;
+    	}
+    	
+    	// Finally, remove isolated nodes from the graph
+    	removeIsolatedNodes();
     }
     
     // Method that eliminate isolated nodes
     private void removeIsolatedNodes()
     {
-        Node currNode = graph.getLast();
-        Node auxNode = null;
-        int n = graph.size();
-        
-        for (int i = 0; i <= n; i++)
-        {
-        	auxNode = currNode.next;
-            if (!currNode.assigned)
-            	graph.remove(currNode);
-            currNode = auxNode;
-        }
-        
+    	Node currNode = graph.getLast();
+    	Node auxNode = null;
+    	int n = graph.size();
+    	
+    	for (int i = 0; i <= n; i++)
+    	{
+    		auxNode = currNode.next;
+    		if (!currNode.assigned)
+    			graph.remove(currNode);
+    		currNode = auxNode;
+    	}
+    	
     }
-
+    
     // This method brings the winning node and its neighbors closer to the stimulus
     private void closeupPhase(int key, int ix)
     {
-        Node node = graph.get(key);
-        
-        // Update target node position
-        node.x += LAMBDA * (doublePoint[ix].x - node.x);
-        node.y += LAMBDA * (doublePoint[ix].y - node.y);
-        
-        // Update right neighbor node position
-        node.previous.x += 0.4 * LAMBDA * (doublePoint[ix].x - node.previous.x);
-        node.previous.y += 0.4 * LAMBDA * (doublePoint[ix].y - node.previous.y);
-        
-        // Update left neighbor node position
-        node.next.x += 0.4 * LAMBDA * (doublePoint[ix].x - node.next.x);
-        node.next.y += 0.4 * LAMBDA * (doublePoint[ix].y - node.next.y);
+    	Node node = graph.get(key);
+    	
+    	// Update target node position
+    	node.x += LAMBDA * (doublePoint[ix].x - node.x);
+    	node.y += LAMBDA * (doublePoint[ix].y - node.y);
+    	
+    	// Update right neighbor node position
+    	node.previous.x += 0.4 * LAMBDA * (doublePoint[ix].x - node.previous.x);
+    	node.previous.y += 0.4 * LAMBDA * (doublePoint[ix].y - node.previous.y);
+    	
+    	// Update left neighbor node position
+    	node.next.x += 0.4 * LAMBDA * (doublePoint[ix].x - node.next.x);
+    	node.next.y += 0.4 * LAMBDA * (doublePoint[ix].y - node.next.y);
     }
     
     // Returns the Euclidean distance between 2 points in the plane
